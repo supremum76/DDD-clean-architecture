ad55b43de5f38a88785e94e441251557e9739d03
Andrey Khomich <supremum76@gmail.com>
2026-07-20 18:30:41 +0300

    http controllers test

--- src/main/java/microarch/delivery/adapters/in/http/CreateOrderApiController.java
+++ src/main/java/microarch/delivery/adapters/in/http/CreateOrderApiController.java
@@ -1,35 +1,16 @@
 package microarch.delivery.adapters.in.http;
 
+import jakarta.annotation.Generated;
 import lombok.RequiredArgsConstructor;
 import microarch.delivery.adapters.in.http.api.CreateOrderApi;
-import microarch.delivery.adapters.in.http.model.CreateCourierResponse;
 import microarch.delivery.adapters.in.http.model.CreateOrderResponse;
-import microarch.delivery.adapters.in.http.model.Error;
 import microarch.delivery.adapters.in.http.model.NewOrder;
-
-
-import microarch.delivery.core.application.commands.CreateCourierCommand;
-import microarch.delivery.core.application.commands.CreateCourierCommandHandler;
 import microarch.delivery.core.application.commands.CreateOrderCommand;
 import microarch.delivery.core.application.commands.CreateOrderCommandHandler;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.context.request.NativeWebRequest;
-
-import jakarta.validation.constraints.*;
-import jakarta.validation.Valid;
-
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import java.util.UUID;
-
-import jakarta.annotation.Generated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2026-07-18T18:31:02.352874400+03:00[Europe/Moscow]", comments = "Generator version: 7.23.0")
 @RestController
@@ -40,12 +21,10 @@ public class CreateOrderApiController implements CreateOrderApi {
 
     @Override
     public ResponseEntity<CreateOrderResponse> createOrder(NewOrder newOrder) {
-        final var orderId = UUID.randomUUID();
-
         // Формируем команду
-        final var address = newOrder.getAddress();
+        var address = newOrder.getAddress();
         var createCommandResult = CreateOrderCommand.create(
-                orderId,
+                newOrder.getId(),
 
                 newOrder.getVolume(),
 
@@ -65,7 +44,7 @@ public class CreateOrderApiController implements CreateOrderApi {
             return ResponseEntity.status(HttpStatus.CONFLICT).build();
 
         // Формируем ответ
-        var response = new CreateOrderResponse(orderId);
+        var response = new CreateOrderResponse(newOrder.getId());
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 }
