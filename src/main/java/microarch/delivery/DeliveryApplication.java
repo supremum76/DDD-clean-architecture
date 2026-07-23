050561287dd517b3781e8e5ccd24ce58ed8191fe
Andrey Khomich <supremum76@gmail.com>
2026-07-23 12:16:09 +0300

    http fix

--- src/main/java/microarch/delivery/DeliveryApplication.java
+++ src/main/java/microarch/delivery/DeliveryApplication.java
@@ -3,13 +3,16 @@ package microarch.delivery;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.FullyQualifiedAnnotationBeanNameGenerator;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @EntityScan(basePackages = {
         "microarch.delivery.core.domain.model",
         "microarch.delivery.adapters.out.postgres" })
-@SpringBootApplication
+@SpringBootApplication(
+        nameGenerator = FullyQualifiedAnnotationBeanNameGenerator.class
+)
 public class DeliveryApplication {
 
     public static void main(String[] args) {
